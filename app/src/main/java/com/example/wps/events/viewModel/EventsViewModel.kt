5b15edbdd6ffa079f71d66bb893f411fa3b07040
AlbertoDoc <alberto-oliveira-santos@hotmail.com>
2022-10-13 15:03:31 -0300

    feature(EventsActivity): Finishing fetching all events.

--- app/src/main/java/com/example/wps/events/viewModel/EventsViewModel.kt
+++ app/src/main/java/com/example/wps/events/viewModel/EventsViewModel.kt
@@ -1,11 +1,20 @@
 package com.example.wps.events.viewModel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.wps.repositories.retrofit.RetrofitClient
+import com.example.wps.repositories.retrofit.services.EventService
 import com.example.wps.repositories.room.database.Database
 import com.example.wps.repositories.room.entities.Event
-import retrofit2.Retrofit
+import com.google.gson.JsonArray
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EventsViewModel : ViewModel() {
 
@@ -15,7 +24,7 @@ class EventsViewModel : ViewModel() {
         }
     }
 
-    private lateinit var retrofitClient: Retrofit
+    private val retrofitClient = RetrofitClient().getRetrofitInstance()
     private lateinit var database: Database
 
     fun loadDatabase(database: Database) {
@@ -27,6 +36,36 @@ class EventsViewModel : ViewModel() {
     }
 
     private fun loadEvents() {
-        // TODO make requisition to fetch events
+        // Load from database to get latest offline results
+        //events.postValue(database.eventDao().getAllLiveData().value)
+
+        val eventService = retrofitClient.create(EventService::class.java)
+        val request = eventService.getAll()
+
+        request.enqueue(object : Callback<JsonArray> {
+            override fun onFailure(call: Call<JsonArray>, t: Throwable) {
+                Log.e("EventsViewModel", t.stackTraceToString())
+            }
+
+            override fun onResponse(call: Call<JsonArray>, response: Response<JsonArray>) {
+                if (response.isSuccessful) {
+                    if (response.code() == 200) {
+                        val responseBody = response.body()
+
+                        if (responseBody != null) {
+                            viewModelScope.launch(Dispatchers.IO) {
+                                for (i in 0 until responseBody.size()) {
+                                    val json = responseBody.get(i).asJsonObject
+                                    val event = Event()
+                                    event.parseJsonObject(json)
+
+                                    database.eventDao().insert(event)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/example/wps/repositories/room/entities/Event.kt
+++ app/src/main/java/com/example/wps/repositories/room/entities/Event.kt
@@ -3,16 +3,39 @@ package com.example.wps.repositories.room.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 
 @Entity
 data class Event (
-    @PrimaryKey val uid: String,
-    @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "date") val date: Long,
-    @ColumnInfo(name = "price") val price: Double,
-    @ColumnInfo(name = "image") val image: String,
-    @ColumnInfo(name = "longitude") val longitude: Double,
-    @ColumnInfo(name = "latitude") val latitude: Double,
-    @ColumnInfo(name = "peoples") val peoples: MutableList<String>
-)
+    @PrimaryKey var uid: String,
+    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "description") var description: String,
+    @ColumnInfo(name = "date") var date: Long,
+    @ColumnInfo(name = "price") var price: Double,
+    @ColumnInfo(name = "image") var image: String,
+    @ColumnInfo(name = "longitude") var longitude: Double,
+    @ColumnInfo(name = "latitude") var latitude: Double,
+    @ColumnInfo(name = "peoples") var peoples: MutableList<String>
+) {
+    constructor() : this("", "", "", 0, 0.0, "", 0.0, 0.0, ArrayList<String>())
+
+    fun parseJsonObject(json: JsonObject) {
+        uid = json.get("id").asString
+        title = json.get("title").asString
+        description = json.get("description").asString
+        date = json.get("date").asLong
+        price = json.get("price").asDouble
+        image = json.get("image").asString
+        longitude = json.get("longitude").asDouble
+        latitude = json.get("latitude").asDouble
+
+        val peoplesList = ArrayList<String>()
+        val peoplesJsonArray = json.getAsJsonArray("people")
+        for (jsonElement: JsonElement in peoplesJsonArray) {
+            peoplesList.add(jsonElement.asString)
+        }
+
+        peoples = peoplesList
+    }
+}
